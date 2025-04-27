971af83aff80aa38e9608984308854254bd94fc9
thegovyadina <1332180+thegovyadina@users.noreply.github.com>
2025-04-27 16:40:04 +0400

    Added problems: 263 - Ugly Number

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -8,6 +8,7 @@ pub mod p0096_unique_binary_search_trees;
 pub mod p0126_word_ladder_ii;
 pub mod p0215_kth_largest_element_in_an_array;
 pub mod p0227_basic_calculator_2;
+pub mod p0263_ugly_number;
 pub mod p0504_base_7;
 pub mod p0552_student_attendance_record_ii;
 pub mod p0645_set_mismatch;
---
+++ src/problems/p0263_ugly_number.rs
@@ -0,0 +1,49 @@
+//! # LeetCode Problem: 263 - Ugly Number
+//!
+//! Difficulty: Easy
+//!
+//! Link: https://leetcode.com/problems/ugly-number/
+//!
+//! ## Complexity Analysis
+//! - Time Complexity: O(log n) - We repeatedly divide the number by 2, 3, and 5
+//!         until it becomes 1 or less.
+//! - Space Complexity: O(1) - We use a constant amount of space for the result.
+
+pub struct Solution;
+
+impl Solution {
+    pub fn is_ugly(n: i32) -> bool {
+        if n <= 0 {
+            return false;
+        }
+        let mut n = n;
+        while n % 2 == 0 {
+            n /= 2;
+        }
+        while n % 3 == 0 {
+            n /= 3;
+        }
+        while n % 5 == 0 {
+            n /= 5;
+        }
+        n == 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_is_ugly() {
+        let test_cases = [(6, true), (1, true), (14, false)];
+        for (idx, (input, expected)) in test_cases.iter().enumerate() {
+            let result = Solution::is_ugly(*input);
+            assert_eq!(
+                result, *expected,
+                "Test case #{}: with input {:?}, expected {:?}, got {:?}",
+                idx, input, expected, result
+            );
+        }
+    }
+}
